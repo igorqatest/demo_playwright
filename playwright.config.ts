62437bf09624050669d5722e7398ff53abdae213
Igor Pejin <pejinigor@gmail.com>
2025-05-17 22:39:05 +0200

    new workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 1 : os.cpus().length,
-  workers: 3,
+  workers: 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['allure-playwright'], // Generates Allure report
